9e3ccf2e204e434e3f4e0ae36f31e2501b1113c9
jonathan <jonathantbrouwer@gmail.com>
2023-06-10 23:56:54 +0200

    Fork rust-gpu-tools

--- src/kernels/gol.cl
+++ src/kernels/gol.cl
@@ -1,5 +1,78 @@
- __kernel void add(uint num, __global uint *a, __global uint *b, __global uint *result) {
-    for (uint i = 0; i < num; i++) {
-      result[i] = a[i] + b[i];
-    }
+/// reduction table
+///        [a0|
+///        |a1|
+///        |a2]
+///        [a3|
+///        |a4|
+///        |a5]
+///        [a6|
+///        |a7]
+/// ---------- +
+///    [b0|[a8|
+///    |b1||a9|
+///    |b2]|aA]
+/// ---------- +
+/// c0 [b3| aB
+///    |b4]
+/// ---------- +
+/// c0  b5  aB
+/// c1
+kernel void step(const global ulong* field, global ulong* new_field, const ulong width)
+{
+    const size_t x = get_global_id(0);
+    const size_t y = get_global_id(1);
+    const size_t i = x + y * width;
+
+    //__local shared_field[64];
+
+    //event_t e = async_work_group_copy(shared_field, field + i, 1, 0);
+    //wait_group_events(1, &e);
+
+    ulong result = field[i];
+
+    // top: left mid right
+    const ulong a0 = (field[i - width] >> 1) | (field[i - width - 1] << 63);
+    const ulong a1 = field[i - width];
+    const ulong a2 = (field[i - width] << 1) | (field[i - width + 1] >> 63);
+
+    // middle: left right
+    const ulong a3 = (result >> 1) | (field[i - 1] << 63);
+    const ulong a4 = (result << 1) | (field[i + 1] >> 63);
+
+    // bottom: left mid right
+    const ulong a5 = (field[i + width] >> 1) | (field[i + width - 1] << 63);
+    const ulong a6 = field[i + width];
+    const ulong a7 = (field[i + width] << 1) | (field[i + width + 1] >> 63);
+
+    // stage 0
+    const ulong ta0 = a0 ^ a1;
+    const ulong a8 = ta0 ^ a2;
+    const ulong b0 = (a0 & a1) | (ta0 & a2);
+
+    const ulong ta3 = a3 ^ a4;
+    const ulong a9 = ta3 ^ a5;
+    const ulong b1 = (a3 & a4) | (ta3 & a5);
+
+    const ulong aA = a6 ^ a7;
+    const ulong b2 = a6 & a7;
+
+    // stage 1
+    const ulong ta8 = a8 ^ a9;
+    const ulong aB = ta8 ^ aA;
+    const ulong b3 = (a8 & a9) | (ta8 & aA);
+
+    const ulong tb0 = b0 ^ b1;
+    const ulong b4 = tb0 ^ b2;
+    const ulong c0 = (b0 & b1) | (tb0 & b2);
+
+    // stage 2
+    const ulong b5 = b3 ^ b4;
+    const ulong c1 = b3 & b4;
+
+    result |= aB;
+    result &= b5;
+    result &= ~c0;
+    result &= ~c1;
+
+    new_field[i] = result;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,8 @@
 mod game;
 
-use rust_gpu_tools::{opencl, program_closures, Device, GPUError, Program};
 
-#[cfg(feature = "cuda")]
-use rust_gpu_tools::cuda;
 
 pub fn main() {
-    // Define some data that should be operated on.
-    let aa: Vec<u32> = vec![1, 2, 3, 4];
-    let bb: Vec<u32> = vec![5, 6, 7, 8];
 
-    let closures = program_closures!(|program, _args| -> Result<Vec<u32>, GPUError> {
-        // Make sure the input data has the same length.
-        assert_eq!(aa.len(), bb.len());
-        let length = aa.len();
-
-        // Copy the data to the GPU.
-        let aa_buffer = program.create_buffer_from_slice(&aa)?;
-        let bb_buffer = program.create_buffer_from_slice(&bb)?;
-
-        // The result buffer has the same length as the input buffers.
-        let result_buffer = unsafe { program.create_buffer::<u32>(length)? };
-
-        // Get the kernel.
-        let kernel = program.create_kernel("add", 1, 1)?;
-
-        // Execute the kernel.
-        kernel
-            .arg(&(length as u32))
-            .arg(&aa_buffer)
-            .arg(&bb_buffer)
-            .arg(&result_buffer)
-            .run()?;
-
-        // Get the resulting data.
-        let mut result = vec![0u32; length];
-        program.read_into_buffer(&result_buffer, &mut result)?;
-
-        Ok(result)
-    });
-
-    let device = Device::all()[0];
-    let program = get_program(device);
-    let result = program.run(closures, ()).unwrap();
-    assert_eq!(result, [6, 8, 10, 12]);
-    println!("Result: {:?}", result);
 }
 
-#[cfg(feature = "cuda")]
-fn get_program(device: &Device) -> Program {
-    // The kernel was compiled with:
-    let cuda_kernel = CString::new("../target/gol.fatbin").unwrap();
-    let cuda_device = device.cuda_device().unwrap();
-    let cuda_program = cuda::Program::from_binary(cuda_device, &cuda_kernel).unwrap();
-    Program::Cuda(cuda_program)
-}
-
-#[cfg(not(feature = "cuda"))]
-fn get_program(device: &Device) -> Program {
-    let opencl_kernel = include_str!("./kernels/gol.cl");
-    let opencl_device = device.opencl_device().unwrap();
-    let opencl_program = opencl::Program::from_opencl(opencl_device, opencl_kernel).unwrap();
-    Program::Opencl(opencl_program)
-}
