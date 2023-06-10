f8b9969d249149c1ec678c4fd06d962e83edbbe5
Vlamonster <jellemartijndijkstra@gmail.com>
2023-06-10 22:59:23 +0200

    Initial commit.

---
+++ build.rs
@@ -0,0 +1,15 @@
+use std::process::Command;
+
+// Example custom build script.
+fn main() {
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    println!("cargo:rerun-if-changed=src/kernels/gol.cu");
+    Command::new("nvcc")
+        .arg("-ptx")
+        .arg("-gencode=arch=compute_61,code=sm_61")
+        .arg("./src/kernels/gol.cu")
+        .arg("-o")
+        .arg("./target/gol.ptx")
+        .spawn()
+        .unwrap();
+}
