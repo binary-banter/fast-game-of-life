d28d15b06c42e137fa58928e2daf8957b4f0e210
Vlamonster <jellemartijndijkstra@gmail.com>
2023-12-29 14:26:59 +0100

    Equivalence test for cuda and trivial implementation. Also upgrade crates.

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,6 @@
 #![cfg_attr(feature = "simd", feature(array_windows))]
 #![cfg_attr(feature = "simd", feature(array_chunks))]
 
-#[cfg(not(any(
-    feature = "opencl",
-    feature = "cuda",
-    feature = "simd",
-    feature = "trivial"
-)))]
-compile_error!("No features were selected!");
-
 #[cfg(all(feature = "opencl", feature = "cuda"))]
 compile_error!("The \"opencl\" and \"cuda\" features are mutually exclusive!");
 
@@ -19,24 +11,10 @@ compile_error!("The \"opencl\" and \"simd\" features are mutually exclusive!");
 #[cfg(all(feature = "cuda", feature = "simd"))]
 compile_error!("The \"cuda\" and \"simd\" features are mutually exclusive!");
 
-#[cfg(feature = "opencl")]
-pub mod opencl;
-#[cfg(feature = "opencl")]
-pub use opencl as game;
-
 #[cfg(feature = "cuda")]
 pub mod cuda;
-#[cfg(feature = "cuda")]
-pub use cuda as game;
-
+#[cfg(feature = "opencl")]
+pub mod opencl;
 #[cfg(feature = "simd")]
 pub mod simd;
-
-#[cfg(feature = "simd")]
-pub use simd as game;
-
-#[cfg(feature = "trivial")]
 pub mod trivial;
-
-#[cfg(feature = "trivial")]
-pub use trivial as game;
